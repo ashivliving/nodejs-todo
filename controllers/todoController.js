88269ea5be112ce27eff5bd1461dd08485a64365
ashivliving <ashivliving@gmail.com>
2017-08-22 23:44:39 +0530

    mongoose added

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -2,21 +2,39 @@ var bodyParser = require('body-parser');
 
 var urlencodedParser = bodyParser.urlencoded({extended : false});
 
-var data = [{item: 'get milk'}, {item: 'do homework'},{item: 'blah blah'}];
+var mongoose = require('mongoose');
+
+//Connect to the database
+mongoose.connect('mongodb://test:test@ds145010.mlab.com:45010/ashiv-todo');
+
+//schema
+var todoSchema = new mongoose.Schema({
+	item: String
+});
+
+var Todo = mongoose.model('Todo', todoSchema);
+
 module.exports = function(app) {
 
 	app.get('/todo', function(req,res){
-		res.render('todo',{todos:data});
+		Todo.find({},function(err,data){
+			if(err) throw err;
+			res.render('todo',{todos:data}); 
+		});
 	});
 
 	app.post('/todo', urlencodedParser, function(req,res){
-		data.push(req.body);
-		res.json(data);
+		var newTodo = Todo(req.body).save(function(err,data){
+			if(err) throw err;
+			console.log('New Item Added!');
+			res.json(data);
+		});
 	});
 
-	app.delete('/todo', function(req,res){
-		data = data.filter(function(todo){
-			return todo.item.replace(/ /g,'-') !== req.params.item;
+	app.delete('/todo/:item', function(req,res){
+		Todo.find({item:req.params.item.replace(/\-/g," ")}).remove(function(err,data){
+			if(err) throw err;
+			res.json(data);
 		});
 	});
 
